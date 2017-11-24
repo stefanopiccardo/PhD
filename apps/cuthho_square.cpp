654a45d277487656a9287c2dbe2c13aaa476befc
Matteo Cicuttin <datafl4sh@toxicnet.eu>
2017-11-24 18:41:33 +0100

    Updated some stuff.

--- apps/cuthho_square.cpp
+++ apps/cuthho_square.cpp
@@ -35,6 +35,8 @@
 
 #include <unistd.h>
 
+#include <silo.h>
+
 using namespace Eigen;
 
 #include "point.hpp"
@@ -44,11 +46,98 @@ struct interface_box {
 
 };
 
+
+class degree_info {
+
+    size_t cell_deg, face_deg;
+
+public:
+    degree_info()
+        : cell_deg(1), face_deg(1)
+    {}
+
+    degree_info(size_t k, int l)
+    {
+        if ( l < -1 || l > 1)
+            l = 0;
+
+        if (l == -1 && k == 0)
+            l = 0;
+
+        cell_deg = k+l;
+        face_deg = k;
+    }
+
+    size_t cell_degree() const
+    {
+        return cell_deg;
+    }
+
+    size_t face_degree() const
+    {
+        return face_deg;
+    }
+};
+
+template<typename T>
+bool
+conjugated_gradient(const Eigen::SparseMatrix<T>& A,
+                    const Eigen::Matrix<T, Eigen::Dynamic, 1>& b,
+                    Eigen::Matrix<T, Eigen::Dynamic, 1>& x)
+{
+    size_t                      N = A.cols();
+    size_t                      iter = 0;
+    T                           nr, nr0;
+    T                           alpha, beta, rho;
+
+    Eigen::Matrix<T, Eigen::Dynamic, 1> d(N), r(N), r0(N), y(N);
+    x = Eigen::Matrix<T, Eigen::Dynamic, 1>::Zero(N);
+
+
+    r0 = d = r = b - A*x;
+    nr = nr0 = r.norm();
+
+    std::ofstream ofs("cg_nopre_convergence.txt");
+
+    while ( nr/nr0 > 1e-8 && iter < 40000 && nr/nr0 < 10000 )
+    {
+        std::cout << "                                                 \r";
+        std::cout << " -> Iteration " << iter << ", rr = ";
+        std::cout << nr/nr0 << "\b\r";
+        std::cout.flush();
+
+        ofs << nr/nr0 << std::endl;
+        y = A*d;
+        rho = r.dot(r);
+        alpha = rho/d.dot(y);
+        x = x + alpha * d;
+        r = r - alpha * y;
+        beta = r.dot(r)/rho;
+        d = r + beta * d;
+
+        nr = r.norm();
+        iter++;
+    }
+
+    ofs << nr/nr0 << std::endl;
+    ofs.close();
+
+    std::cout << " -> Iteration " << iter << ", rr = " << nr/nr0 << std::endl;
+
+    return true;
+}
+
 /*****************************************************************************
  *   Mesh stuff
  *****************************************************************************/
 struct cell {
     std::array<size_t, 4>   ptids;
+
+    bool cut_by_interface;
+
+    cell()
+        : cut_by_interface(false)
+    {}
 
     bool operator<(const cell& other) const
     {
@@ -208,6 +297,140 @@ struct mesh {
         faces.erase( std::unique(faces.begin(), faces.end()), faces.end() );
     }
 };
+
+/*****************************************************************************
+ *   SILO stuff
+ *****************************************************************************/
+
+enum variable_centering_t
+{
+    nodal_variable_t,
+    zonal_variable_t
+};
+
+
+class silo_database
+{
+    DBfile          *m_siloDb;
+
+public:
+    silo_database()
+        : m_siloDb(nullptr)
+    {}
+
+    bool create(const std::string& db_name)
+    {
+        m_siloDb = DBCreate(db_name.c_str(), DB_CLOBBER, DB_LOCAL, NULL, DB_PDB);
+        if (m_siloDb)
+            return true;
+
+        std::cout << "Error creating database" << std::endl;
+        return false;
+    }
+
+    bool open(const std::string& db_name)
+    {
+        m_siloDb = DBOpen(db_name.c_str(), DB_PDB, DB_APPEND);
+        if (m_siloDb)
+            return true;
+
+        std::cout << "Error opening database" << std::endl;
+        return false;
+    }
+
+    bool close()
+    {
+        if (m_siloDb)
+            DBClose(m_siloDb);
+        m_siloDb = NULL;
+        return true;
+    }
+
+    ~silo_database()
+    {
+        if (m_siloDb)
+            DBClose(m_siloDb);
+    }
+
+    template<typename T>
+    bool add_mesh(const mesh<T>& msh, const std::string& name)
+    {
+        static_assert(std::is_same<T,double>::value, "Only double for now");
+
+        std::vector<T> x_coords, y_coords;
+        x_coords.reserve(msh.points.size());
+        y_coords.reserve(msh.points.size());
+
+        for (auto itor = msh.points.begin(); itor != msh.points.end(); itor++)
+        {
+            auto pt = *itor;
+            x_coords.push_back(pt.x());
+            y_coords.push_back(pt.y());
+        }
+
+        T *coords[] = {x_coords.data(), y_coords.data()};
+
+        std::vector<int>    nodelist;
+
+        for (auto& cl : msh.cells)
+        {
+            for (auto& ptid : cl.ptids)
+                nodelist.push_back(ptid+1);
+        }
+
+        int lnodelist       = nodelist.size();
+        int nshapetypes     = 1;
+        int shapesize       = 4;
+        int shapecount      = msh.cells.size();
+
+
+        int nnodes = msh.points.size();
+        int nzones = msh.cells.size();
+        int ndims = 2;
+
+        DBPutZonelist(m_siloDb, "zonelist", nzones, ndims, nodelist.data(), lnodelist,
+            1, &shapesize, &shapecount, nshapetypes);
+
+        DBPutUcdmesh(m_siloDb, name.c_str(), ndims, NULL, coords, nnodes, nzones,
+            "zonelist", NULL, DB_DOUBLE, NULL);
+
+        return true;
+    }
+
+    template<typename T>
+    bool add_variable(const std::string& mesh_name,
+                      const std::string& var_name,
+                      const T* data,
+                      size_t data_len,
+                      variable_centering_t centering)
+    {
+        static_assert(std::is_same<T,double>::value, "Only double for now");
+
+        if (!m_siloDb)
+        {
+            std::cout << "Silo database not opened" << std::endl;
+            return false;
+        }
+
+        if (centering == zonal_variable_t)
+        {
+            DBPutUcdvar1(m_siloDb, var_name.c_str(), mesh_name.c_str(),
+                         data, data_len, NULL, 0, DB_DOUBLE, DB_ZONECENT, NULL);
+            return true;
+        }
+
+        if (centering == nodal_variable_t)
+        {
+            DBPutUcdvar1(m_siloDb, var_name.c_str(), mesh_name.c_str(),
+                         data, data_len, NULL, 0, DB_DOUBLE, DB_NODECENT, NULL);
+            return true;
+        }
+
+        return false;
+    }
+
+};
+
 
 /*****************************************************************************
  *   Geometry
@@ -379,6 +602,32 @@ normals(const mesh<T>& msh, const typename mesh<T>::cell_type& cl)
     ret[3](1) = 0;
 
     return ret;
+}
+
+template<typename T, typename Function>
+void
+detect_cut_cells(mesh<T>& msh, const Function& level_set_function)
+{
+    for (auto& cl : msh.cells)
+    {
+        auto pts = points(msh, cl);
+        std::vector<int> signs;
+        signs.resize(pts.size());
+
+        auto compute_sign = [&](typename mesh<T>::point_type pt) -> int {
+            return level_set_function(pt) < 0 ? -1 : +1;
+        };
+
+        std::transform(pts.begin(), pts.end(), signs.begin(), compute_sign);
+
+        cl.cut_by_interface = true;
+
+        if ( std::count(signs.begin(), signs.end(), 1) == signs.size() )
+            cl.cut_by_interface = false;
+
+        if ( std::count(signs.begin(), signs.end(), -1) == signs.size() )
+            cl.cut_by_interface = false;
+    }
 }
 
 template<typename T>
@@ -918,6 +1167,13 @@ make_hho_laplacian(const mesh<T>& msh, const typename mesh<T>::cell_type& cl, si
 }
 
 template<typename T>
+std::pair<Matrix<T, Dynamic, Dynamic>, Matrix<T, Dynamic, Dynamic>>
+make_operator(const mesh<T>& msh, const typename mesh<T>::cell_type& cl, size_t degree)
+{
+    return make_hho_laplacian(msh, cl, degree);
+}
+
+template<typename T>
 Matrix<T, Dynamic, Dynamic>
 make_hho_naive_stabilization(const mesh<T>& msh, const typename mesh<T>::cell_type& cl, size_t degree)
 {
@@ -959,6 +1215,13 @@ make_hho_naive_stabilization(const mesh<T>& msh, const typename mesh<T>::cell_ty
     }
 
     return data;
+}
+
+template<typename T>
+Matrix<T, Dynamic, Dynamic>
+make_stabilization(const mesh<T>& msh, const typename mesh<T>::cell_type& cl, size_t degree)
+{
+    return make_hho_naive_stabilization(msh, cl, degree);
 }
 
 /*****************************************************************************
@@ -1233,6 +1496,8 @@ public:
             }
         }
 
+        assert( asm_map.size() == lhs.rows() && asm_map.size() == lhs.cols() );
+
         for (size_t i = 0; i < lhs.rows(); i++)
         {
             if (!asm_map[i].assemble())
@@ -1247,8 +1512,46 @@ public:
             }
         }
 
-        RHS.block( cell_LHS_offset, 0, cbs, 1) += rhs.block(0, 0, cbs, 1);
+        RHS.block(cell_LHS_offset, 0, cbs, 1) += rhs.block(0, 0, cbs, 1);
     } // assemble()
+
+    template<typename Function>
+    Matrix<T, Dynamic, 1>
+    take_local_data(const mesh<T>& msh, const typename mesh<T>::cell_type& cl,
+    const Matrix<T, Dynamic, 1>& solution, const Function& dirichlet_bf)
+    {
+        auto cbs = cell_basis<T,T>::size(degree);
+        auto fbs = face_basis<T,T>::size(degree);
+
+        auto cell_offset        = offset(msh, cl);
+        auto cell_SOL_offset    = cell_offset * cbs;
+
+        Matrix<T, Dynamic, 1> ret = Matrix<T, Dynamic, 1>::Zero(cbs + 4*fbs);
+        ret.block(0, 0, cbs, 1) = solution.block(cell_SOL_offset, 0, cbs, 1);
+
+        auto fcs = faces(msh, cl);
+        for (size_t face_i = 0; face_i < 4; face_i++)
+        {
+            auto fc = fcs[face_i];
+
+            bool dirichlet = fc.is_boundary && fc.bndtype == boundary::DIRICHLET;
+
+            if (dirichlet)
+            {
+                Matrix<T, Dynamic, Dynamic> mass = make_mass_matrix(msh, fc, degree);
+                Matrix<T, Dynamic, 1> rhs = make_rhs(msh, fc, degree, dirichlet_bf);
+                ret.block(cbs+face_i*fbs, 0, fbs, 1) = mass.llt().solve(rhs);
+            }
+            else
+            {
+                auto face_offset = offset(msh, fc);
+                auto face_SOL_offset = cbs * msh.cells.size() + compress_table.at(face_offset)*fbs;
+                ret.block(cbs+face_i*fbs, 0, fbs, 1) = solution.block(face_SOL_offset, 0, fbs, 1);
+            }
+        }
+
+        return ret;
+    }
 
     void finalize(void)
     {
@@ -1326,6 +1629,20 @@ void dump_mesh(const mesh<T>& msh)
 }
 
 
+template<typename T>
+void add_interface_edges(const mesh<T>& msh)
+{
+    for (auto& cl : msh.cells)
+    {
+        auto fcs = faces(msh, cl);
+        for (auto& fc : fcs)
+        {
+            auto pts = points(msh, fc);
+            auto len = (pts[1] - pts[0]).to_vector().norm();
+        }
+    }
+}
+
 
 int main(int argc, char **argv)
 {
@@ -1367,6 +1684,41 @@ int main(int argc, char **argv)
 
     dump_mesh(msh);
 
+    silo_database silo;
+    silo.create("cuthho_square.silo");
+    silo.add_mesh(msh, "mesh");
+
+
+
+    auto level_set_function = [](const typename mesh<RealType>::point_type pt) -> RealType {
+        auto x = pt.x();
+        auto y = pt.y();
+        auto alpha = 0.5;
+        auto beta = 0.5;
+
+        return (x-alpha)*(x-alpha) + (y-beta)*(y-beta) - 0.15;
+    };
+
+    detect_cut_cells(msh, level_set_function);
+
+    std::vector<RealType> cut_cell_markers;
+    for (auto& cl : msh.cells)
+    {
+        if (cl.cut_by_interface)
+            cut_cell_markers.push_back(1.0);
+        else
+            cut_cell_markers.push_back(0.0);
+    }
+    silo.add_variable("mesh", "cut_cells", cut_cell_markers.data(), cut_cell_markers.size(), zonal_variable_t);
+
+
+    std::vector<RealType> level_set_vals;
+    for (auto& pt : msh.points)
+        level_set_vals.push_back( level_set_function(pt) );
+    silo.add_variable("mesh", "level_set", level_set_vals.data(), level_set_vals.size(), nodal_variable_t);
+
+
+
     auto rhs_fun = [](const typename mesh<RealType>::point_type& pt) -> RealType {
         return 2.0 * M_PI * M_PI * std::sin(M_PI*pt.x()) * std::sin(M_PI*pt.y());
         //return pt.x() * pt.x() * pt.x();
@@ -1383,15 +1735,13 @@ int main(int argc, char **argv)
             fc.bndtype = boundary::DIRICHLET;
     }
 
-
     auto assembler = make_assembler(msh, degree);
-
     for (auto& cl : msh.cells)
     {
-        auto gr = make_hho_laplacian(msh, cl, degree);
-        Matrix<RealType, Dynamic, Dynamic> stab = make_hho_naive_stabilization(msh, cl, degree);
+        auto gr = make_operator(msh, cl, degree);
+        Matrix<RealType, Dynamic, Dynamic> stab = make_stabilization(msh, cl, degree);
         Matrix<RealType, Dynamic, Dynamic> lc = gr.second + stab;
-        Matrix<RealType, Dynamic, 1> ft = project_function(msh, cl, degree, rhs_fun);
+        //Matrix<RealType, Dynamic, 1> ft = project_function(msh, cl, degree, rhs_fun);
         Matrix<RealType, Dynamic, 1> f = make_rhs(msh, cl, degree, rhs_fun);
         assembler.assemble(msh, cl, lc, f, sol_fun);
     }
@@ -1414,16 +1764,28 @@ int main(int argc, char **argv)
     std::cout << "Cells: " << msh.cells.size() << std::endl;
     std::cout << "Faces: " << msh.faces.size() << std::endl;
 
+//#if 0
     SparseLU<SparseMatrix<RealType>>  solver;
 
     solver.analyzePattern(assembler.LHS);
     solver.factorize(assembler.LHS);
     Matrix<RealType, Dynamic, 1> sol = solver.solve(assembler.RHS);
+//#endif
+#if 0
+    Matrix<RealType, Dynamic, 1> sol = Matrix<RealType, Dynamic, 1>::Zero(assembler.RHS.rows());
+    conjugated_gradient(assembler.LHS, assembler.RHS, sol);
+#endif
 
     std::ofstream ofs("solution.dat");
-    size_t cell_i;
     RealType error_int = 0.0;
     RealType error_mm = 0.0;
+
+    std::vector<RealType>   error_int_percell;
+    std::vector<RealType>   error_mm_percell;
+    std::vector<RealType>   solution_val;
+    std::vector<RealType>   stab_val;
+
+    size_t cell_i = 0;
     for (auto& cl : msh.cells)
     {
         cell_basis<RealType,RealType> cb(msh, cl, degree);
@@ -1439,25 +1801,46 @@ int main(int argc, char **argv)
             ofs << tp.x() << " " << tp.y() << " " << val << std::endl;
         }
 
-
+        RealType e_int_c = 0.0;
         auto qps = integrate(msh, cl, 2*degree);
         for (auto& qp : qps)
         {
             Matrix<RealType, Dynamic, 1> phi = cb.eval_basis(qp.first);
             auto val = cdofs.dot( phi );
             auto real_val = sol_fun( qp.first );
-            error_int += qp.second*(real_val - val)*(real_val - val);
+            e_int_c += qp.second*(real_val - val)*(real_val - val);
         }
+        error_int_percell.push_back(e_int_c);
+        error_int += e_int_c;
 
         Matrix<RealType, Dynamic, Dynamic> mass = make_mass_matrix(msh, cl, degree);
         Matrix<RealType, Dynamic, 1> rhs = make_rhs(msh, cl, degree, sol_fun);
         Matrix<RealType, Dynamic, 1> real_dofs = mass.llt().solve(rhs);
         Matrix<RealType, Dynamic, 1> diff = real_dofs - cdofs;
-        error_mm += diff.dot(mass*diff);
+        RealType e_mm_c = diff.dot(mass*diff);
+        error_mm_percell.push_back(e_mm_c);
+        error_mm += e_mm_c;
+
+        {
+            auto bar = barycenter(msh, cl);
+            Matrix<RealType, Dynamic, 1> phi = cb.eval_basis(bar);
+            auto val = cdofs.dot( phi );
+            solution_val.push_back(val);
+        }
+
+        Matrix<RealType, Dynamic, Dynamic> stab = make_stabilization(msh, cl, degree);
+        Matrix<RealType, Dynamic, 1> c_alldofs = assembler.take_local_data(msh, cl, sol, sol_fun);
+        auto sv = c_alldofs.dot(stab*c_alldofs);
+        stab_val.push_back(sv);
 
         cell_i++;
     }
     ofs.close();
+
+    silo.add_variable("mesh", "err_int_c", error_int_percell.data(), error_int_percell.size(), zonal_variable_t);
+    silo.add_variable("mesh", "err_mm_c", error_mm_percell.data(), error_mm_percell.size(), zonal_variable_t);
+    silo.add_variable("mesh", "u", solution_val.data(), solution_val.size(), zonal_variable_t);
+    silo.add_variable("mesh", "stab_val", stab_val.data(), stab_val.size(), zonal_variable_t);
 
     std::cout << "L2-error (int): " << sqrt(error_int) << std::endl;
     std::cout << "L2-error (mm) : " << sqrt(error_mm) << std::endl;
