cac88672b45c7d436e3291530089eaf0916737a3
gdelay <gui.delay@laposte.net>
2019-09-27 16:56:34 +0200

    matrix_cell_basis

--- src/core/core_bits/bases.hpp
+++ src/core/core_bits/bases.hpp
@@ -474,3 +474,80 @@ public:
         return 2*(degree+1);
     }
 };
+
+
+////////////////////  MATRIX BASIS  /////////////////////////
+
+template<typename Mesh, typename VT>
+class matrix_cell_basis
+{
+    typedef typename Mesh::coordinate_type  coordinate_type;
+    typedef typename Mesh::point_type       point_type;
+    typedef Matrix<VT, 2, 2>                matrix_type;
+    typedef Matrix<VT, Dynamic, 2>          function_type;
+
+    point_type          cell_bar;
+    coordinate_type     cell_h;
+    size_t              basis_degree, basis_size;
+
+    cell_basis<Mesh,VT>          scalar_basis;
+
+#ifdef POWER_CACHE
+    std::vector<coordinate_type>  power_cache;
+#endif
+
+public:
+    matrix_cell_basis(const Mesh& msh, const typename Mesh::cell_type& cl, size_t degree) :
+        scalar_basis(msh, cl, degree)
+    {
+        cell_bar        = barycenter(msh, cl);
+        cell_h          = diameter(msh, cl);
+        basis_degree    = degree;
+        basis_size      = 2*2*(basis_degree+2)*(basis_degree+1)/2;
+    }
+
+    std::vector<matrix_type>
+    eval_basis(const point_type& pt)
+    {
+        std::vector<matrix_type> ret;
+        ret.reserve(basis_size);
+
+        const auto phi = scalar_basis.eval_basis(pt);
+
+
+
+        for (int k = 0; k < scalar_basis.size(); k++)
+        {
+            matrix_type fc;
+
+            for (int j = 0; j < 2; j++)
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    fc       = matrix_type::Zero();
+                    fc(i, j) = phi(k);
+                    ret.push_back(fc);
+                }
+            }
+        }
+
+        assert(2 * 2 * scalar_basis.size() == basis_size);
+
+        return ret;
+    }
+
+    size_t size() const
+    {
+        return basis_size;
+    }
+
+    size_t degree() const
+    {
+        return basis_degree;
+    }
+
+    static size_t size(size_t degree)
+    {
+        return 2*2*(degree+2)*(degree+1)/2;
+    }
+};
