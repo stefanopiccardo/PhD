0cb62a27c4a6f4b5acfe820222a8864ac04664aa
gdelay <gui.delay@laposte.net>
2019-09-30 10:40:02 +0200

    vector test_case with jumps

--- src/core/core_bits/test_cases.hpp
+++ src/core/core_bits/test_cases.hpp
@@ -676,3 +676,86 @@ auto make_test_case_vector_laplacian_sin_sin_exp_cos(const Mesh& msh, Function l
 {
     return test_case_vector_laplacian_sin_sin_exp_cos<typename Mesh::coordinate_type, Function, Mesh>(level_set_function);
 }
+
+
+///// test_case_vector_laplacian_jumps_1
+// exact solution : sin(\pi x) sin(\pi y)  in \Omega_1  (for every component)
+//                  exp(x) * cos(y)        in \Omega_2  (for every component)
+// \kappa_1 = \kappa_2 = 1
+template<typename T, typename Function, typename Mesh>
+class test_case_vector_laplacian_jumps_1: public test_case_vector_laplacian<T, Function, Mesh>
+{
+   public:
+    test_case_vector_laplacian_jumps_1(Function level_set__)
+        : test_case_vector_laplacian<T, Function, Mesh>
+        (level_set__, params<T>(),
+         [level_set__](const typename Mesh::point_type& pt) -> Eigen::Matrix<T, 2, 1> { /* sol */
+            Matrix<T, 2, 1> ret;
+            if(level_set__(pt) < 0)
+            {
+                ret(0) = std::sin(M_PI*pt.x()) * std::sin(M_PI*pt.y());
+                ret(1) = std::sin(M_PI*pt.x()) * std::sin(M_PI*pt.y());
+            }
+            else
+            {
+                ret(0) = std::exp(pt.x()) * std::cos(pt.y());
+                ret(1) = std::exp(pt.x()) * std::cos(pt.y());
+            }
+            return ret;},
+         [level_set__](const typename Mesh::point_type& pt) -> Eigen::Matrix<T, 2, 1> { /* rhs */
+             Matrix<T, 2, 1> ret;
+             if(level_set__(pt) < 0)
+             {
+                 ret(0) = 2.0 * M_PI * M_PI * std::sin(M_PI*pt.x()) * std::sin(M_PI*pt.y());
+                 ret(1) = 2.0 * M_PI * M_PI * std::sin(M_PI*pt.x()) * std::sin(M_PI*pt.y());
+             } // else -> 0
+             return ret;},
+         [level_set__](const typename Mesh::point_type& pt) -> Eigen::Matrix<T, 2, 1> { // bcs
+            Matrix<T, 2, 1> ret;
+            if(level_set__(pt) < 0)
+            {
+                ret(0) = std::sin(M_PI*pt.x()) * std::sin(M_PI*pt.y());
+                ret(1) = std::sin(M_PI*pt.x()) * std::sin(M_PI*pt.y());
+            }
+            else
+            {
+                ret(0) = std::exp(pt.x()) * std::cos(pt.y());
+                ret(1) = std::exp(pt.x()) * std::cos(pt.y());
+            }
+            return ret;},
+         [level_set__](const typename Mesh::point_type& pt) -> auto { // grad
+             Matrix<T, 2, 2> ret;
+             if(level_set__(pt) < 0)
+             {
+                 ret(0,0) = M_PI * std::cos(M_PI*pt.x()) * std::sin(M_PI*pt.y());
+                 ret(0,1) = M_PI * std::sin(M_PI*pt.x()) * std::cos(M_PI*pt.y());
+                 ret(1,0) = M_PI * std::cos(M_PI*pt.x()) * std::sin(M_PI*pt.y());
+                 ret(1,1) = M_PI * std::sin(M_PI*pt.x()) * std::cos(M_PI*pt.y());
+             }
+             else {
+                 ret(0,0) = exp(pt.x()) * std::cos(pt.y());
+                 ret(0,1) = - exp(pt.x()) * std::sin(pt.y());
+                 ret(1,0) = exp(pt.x()) * std::cos(pt.y());
+                 ret(1,1) = - exp(pt.x()) * std::sin(pt.y());
+             }
+             return ret;},
+         [](const typename Mesh::point_type& pt) -> Eigen::Matrix<T, 2, 1> {/* Dir */
+             Matrix<T, 2, 1> ret;
+             ret(0) = std::sin(M_PI*pt.x()) * std::sin(M_PI*pt.y()) - exp(pt.x()) * std::cos(pt.y());
+             ret(1) = std::sin(M_PI*pt.x()) * std::sin(M_PI*pt.y()) - exp(pt.x()) * std::cos(pt.y());             
+             return ret;},
+         [level_set__](const typename Mesh::point_type& pt) -> Eigen::Matrix<T, 2, 1> {/* Neu */
+             Matrix<T, 1, 2> normal = level_set__.normal(pt);
+             Matrix<T, 2, 1> ret;
+             T sca_ret = (M_PI * std::cos(M_PI*pt.x()) * std::sin(M_PI*pt.y()) - exp(pt.x()) * std::cos(pt.y())) * normal(0) + ( M_PI * std::sin(M_PI*pt.x()) * std::cos(M_PI*pt.y()) + exp(pt.x()) * std::sin(pt.y()) ) * normal(1);
+             ret(0) = sca_ret;
+             ret(1) = sca_ret;
+             return ret;})
+        {}
+};
+
+template<typename Mesh, typename Function>
+auto make_test_case_vector_laplacian_jumps_1(const Mesh& msh, Function level_set_function)
+{
+    return test_case_vector_laplacian_jumps_1<typename Mesh::coordinate_type, Function, Mesh>(level_set_function);
+}
